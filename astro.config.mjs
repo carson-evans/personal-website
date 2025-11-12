5c380011407c6f2377f8046cd1570d56c82644bc
carson-evans <carsonevans@outlook.com>
2025-11-12 09:40:46 -0500

    Add Layout.astro, static build works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+﻿import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://carson-evans.net",
+  output: "static",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
 });
